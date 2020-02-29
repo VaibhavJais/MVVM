9265b78a01c913f073dd21882c57a26f9bf519ac
Oscar Cardona <ocardona@omitsis.com>
2020-02-29 15:26:57 +0100

    Implement MainList Decodable

--- MVVM/Data/Decodables/MainList+Decodable.swift
+++ MVVM/Data/Decodables/MainList+Decodable.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+extension MainListEntity: Decodable {
+
+    private enum CondigKeys: String, CodingKey {
+        case items
+    }
+
+    public init(from decoder: Decoder) throws {
+        let data = try decoder.singleValueContainer()
+        self.items = try data.decode([MainItemEntity].self)
+    }
+}
+
+extension MainItemEntity: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case imageUrl
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.imageUrl = try container.decodeIfPresent(String.self, forKey: .imageUrl)
+    }
+}
